0a93b7bf3982a98bd75ecfd57c3ecb12f2b60ada
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-25 13:15:06 +0530

    Added : Dependencies for mockito in pom.xml

---
+++ src/test/java/com/monckintoBusiness/MockintoBusiness/FirstMockintoTest.java
@@ -0,0 +1,14 @@
+package com.monckintoBusiness.MockintoBusiness;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstMockintoTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
